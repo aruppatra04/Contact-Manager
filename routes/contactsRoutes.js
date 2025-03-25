5399a96848a7dd67dd4ae731e878173d4eb5ca16
Arup Patra <aruppatra2004@gmail.com>
2025-03-26 05:23:14 +0530

    contactsRoutes.js file modified

--- routes/contactsRoutes.js
+++ routes/contactsRoutes.js
@@ -7,15 +7,13 @@ const {
     updateContact, 
     deleteContact 
 } = require("../controllers/contactController");
+const validateToken = require('../middleware/validateTokenHandler');
 
-router.route('/').get(getContacts); 
+router.use(validateToken);
 
-router.route('/').post(createContact);
+router.route('/').get(getContacts).post(createContact); 
 
-router.route('/:id').get(getContact);
+router.route('/:id').get(getContact).put(updateContact).delete(deleteContact);
 
-router.route('/:id').put(updateContact);
-
-router.route('/:id').delete(deleteContact); 
 
 module.exports = router;
